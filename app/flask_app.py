086b84da955d6d5e13aa5644fbed654966534d27
Левченко Алексей Сергеевич <Levchenko.A.Sergeevic@sberbank.ru>
2020-05-23 22:34:13 +0300

    gipo refactor 2

--- app/flask_app.py
+++ app/flask_app.py
@@ -27,12 +27,53 @@ def index():
 
 
 @app.route('/gipo', methods=['POST', 'GET'])
-def outer_gipo_handler():
-    if request.method == 'POST':
-        msg = request.get_json()
-        parsed = parse_message(msg)
-        inner_gipo_handler(parsed)
-        return Response('Ok', status=200)
+def gipo_handler():
+
+   if request.method == 'POST':
+       msg = request.get_json()
+       parsed = parse_message(msg)
+
+       if (not parsed['txt']):
+           send_message(parsed['chat_id'], gipo_token, '...')
+           return Response('Ok', status=200)
+
+       elif parsed['txt'] == '/calc':
+           send_message(parsed['chat_id'], gipo_token, 'Ввведите через разделитель следующие данные')
+           send_message(parsed['chat_id'], gipo_token,
+                        'стоимость объекта / первоначальный взнос в % / процентную ставку в % / количество лет ипотеки')
+           send_message(parsed['chat_id'], gipo_token, 'Пример:')
+           send_message(parsed['chat_id'], gipo_token, '3000000 / 20 / 10 / 30')
+           return Response('Ok', status=200)
+
+       elif len(parsed['txt'].split('/')) == 4:
+           vals = parsed['txt'].split('/')
+           m = Mortgage()
+           m.home_value = int(vals[0])
+           m.down_payment_percent = float(vals[1]) / 100
+           vals[2] = vals[2].replace(',', '.')
+           m.mortgage_rate = float(vals[2]) / 100
+           m.years = int(vals[3])
+           m.run()
+           send_message(parsed['chat_id'], gipo_token, f'Cтоимость объекта: {m.home_value}')
+           send_message(parsed['chat_id'], gipo_token, f'Первоначальный взнос в % : {m.down_payment_percent * 100}')
+           send_message(parsed['chat_id'], gipo_token, f'Процентная ставка в %: {m.mortgage_rate * 100}')
+           send_message(parsed['chat_id'], gipo_token, f'Первоначальный взнос: {m.down_payment}')
+           send_message(parsed['chat_id'], gipo_token, f'Сумма кредита: {m.mortgage_loan}')
+           send_message(parsed['chat_id'], gipo_token, f'Процентная ставка по кредиту : {m.mortgage_rate * 100}%')
+           send_message(parsed['chat_id'], gipo_token, f'Количество лет кредита: {m.years}')
+           send_message(parsed['chat_id'], gipo_token, f'Количетсво периодов платежей: {m.mortgage_payment_periods}')
+           send_message(parsed['chat_id'], gipo_token,
+                        f"Ежемесячный платеж по кредиту: {round(m.periodic_mortgage_payment, 2)}")
+           send_message(parsed['chat_id'], gipo_token, f"Первый платеж (проценты): {round(m.initial_interest_payment, 2)}")
+           send_message(parsed['chat_id'], gipo_token,
+                        f"Первый платеж (основной долг): {round(m.initial_principal_payment, 2)}")
+
+           return Response('Ok', status=200)
+
+       else:
+           send_message(parsed['chat_id'], gipo_token, '.....')
+           return Response('Ok', status=200)
+
     else:
         return '<h1>Gipo</h1>'
 
